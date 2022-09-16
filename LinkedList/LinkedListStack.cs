dbcd8c1c331b701a73082df7cd7954fc4c0bb5e7
AnshumanMunmun <imanshuman47@gmail.com>
2022-09-16 23:16:37 +0530

    [Anshuman] Add: Create a Stack

--- LinkedList/LinkedListStack.cs
+++ LinkedList/LinkedListStack.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace LinkedList
 {
-    internal class LinkedListStack
+    public class LinkedListStack
     {
+        private Node top;                     //Create Stack
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+        internal void Push(int value)                //Push Operion in stack list
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
     }
 }
